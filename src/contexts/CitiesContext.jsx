6ed7fc38c046d88606c87fb3c43a9e7f41970b7f
Hamza Baig <hamzamunawar345@gmail.com>
2024-09-06 00:56:49 +0500

    map started

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,54 +1,54 @@
 import { useContext, useState, useEffect, createContext } from "react";
 
 const CitiesContext = createContext();
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "http://localhost:9000";
 
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+	const [cities, setCities] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [currentCity, setCurrentCity] = useState({});
 
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was some error");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
+	useEffect(function () {
+		async function fetchCities() {
+			try {
+				setIsLoading(true);
+				const res = await fetch(`${BASE_URL}/cities`);
+				const data = await res.json();
+				setCities(data);
+			} catch {
+				alert("There was some error");
+			} finally {
+				setIsLoading(false);
+			}
+		}
+		fetchCities();
+	}, []);
 
-  async function getCity(id) {
-    try {
-      setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      setCurrentCity(data);
-    } catch {
-      alert("There was some error");
-    } finally {
-      setIsLoading(false);
-    }
-  }
+	async function getCity(id) {
+		try {
+			setIsLoading(true);
+			const res = await fetch(`${BASE_URL}/cities/${id}`);
+			const data = await res.json();
+			setCurrentCity(data);
+		} catch {
+			alert("There was some error");
+		} finally {
+			setIsLoading(false);
+		}
+	}
 
-  return (
-    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
-      {children}
-    </CitiesContext.Provider>
-  );
+	return (
+		<CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+			{children}
+		</CitiesContext.Provider>
+	);
 }
 
 function useCities() {
-  const context = useContext(CitiesContext);
-  if (context === undefined)
-    throw new Error("CitiesContext was used outside of CitiesProvider");
-  return context;
+	const context = useContext(CitiesContext);
+	if (context === undefined)
+		throw new Error("CitiesContext was used outside of CitiesProvider");
+	return context;
 }
 
 export { CitiesProvider, useCities };
